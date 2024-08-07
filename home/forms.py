d427b890ac3db276be104d243e1ac9dfd5501c4a
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-08-07 08:23:49 -0300

    feat(forms.py): add forms for user registration

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,33 @@
 from django import forms
-from .models import PlantedTree
+from .models import PlantedTree, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class PlantedTreeForm(forms.ModelForm):
+    """
+    Form for adding or updating a PlantedTree instance.
+    """
+
     class Meta:
         model = PlantedTree
         fields = ["tree", "age", "latitude", "longitude", "account"]
+
+
+class UserRegistrationForm(UserCreationForm):
+    """
+    Form for user registration, extending Django's UserCreationForm.
+    """
+
+    about = forms.CharField(required=False, widget=forms.Textarea)
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "about")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            Profile.objects.create(user=user, about=self.cleaned_data["about"])
+        return user
