3bc2c1ea2e1799f2d78781a07337174cf7556021
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-08-07 08:26:41 -0300

    docs(serializers.py): add comments

--- home/serializers.py
+++ home/serializers.py
@@ -3,6 +3,10 @@ from .models import PlantedTree
 
 
 class PlantedTreeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the PlantedTree model.
+    """
+
     class Meta:
         model = PlantedTree
         fields = ["id", "tree", "age", "latitude", "longitude", "planted_at", "account"]
