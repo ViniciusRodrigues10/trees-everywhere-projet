fc38e2955e9c162e335d3672c6d246e603aa0c90
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-08-07 13:09:29 -0300

    feat(urls.py): add the routes to swagger documentation

--- trees_everywhere/urls.py
+++ trees_everywhere/urls.py
@@ -16,10 +16,36 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from home import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+        description="API documentation for the Trees Everywhere project",
+        contact=openapi.Contact(email="viniciusgcrodrigues.contato@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
